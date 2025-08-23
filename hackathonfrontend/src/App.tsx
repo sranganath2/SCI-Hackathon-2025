5ed10eb346c9afa951dc38c55055d3fc01e7e8fd
Siddarth Ranganath <sranganath7@gatech.edu>
2025-08-23 19:05:19 -0400

    made a login for u jason

--- hackathonfrontend/src/App.tsx
+++ hackathonfrontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BLEControl from "./BLEControl";
+import Login from "./login"
 function App() {
   return (
     <div className="App">
       <div>
+    <Login />
     <BLEControl />
       </div>
     </div>
